63384874a2b9cf72ccc336bed62160bde30be502
Santhosh <santhoshsunthar@gmail.com>
2023-12-14 12:26:14 +0530

    Added JsonFileHandler changes

--- Rbcapp1ServicesMonitor/src/json_file_handler.py
+++ Rbcapp1ServicesMonitor/src/json_file_handler.py
@@ -1,12 +1,25 @@
 import json
+import socket
+from datetime import datetime
+import os
 
 class JsonFileHandler:
-    def createFile(**services):
-        for service in services:
+    def createLog(service, status):
             serviceLog = {
-                "service_name": services["httpd"],
-                "service_status": services["postgres"],
-                "service_hostname": services["rabbitmq"],
+                "service_name": service,
+                "service_status": status,
+                "service_hostname": socket.gethostname(),
             }
          
             return json.dumps(serviceLog, indent=2)
+
+    def createFile(service, serviceLog):
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        folderName = "service_logs"
+        fileName = f"{service}-status-@{timestamp}.json"
+
+        if not os.path.exists(folderName):
+            os.makedirs(folderName)
+        
+        with open(os.path.join(folderName, fileName), "w") as serviceLogFile:
+            json.dump(serviceLog, serviceLogFile, indent=2)
--- Rbcapp1ServicesMonitor/src/rbcapp1_services_monitor.py
+++ Rbcapp1ServicesMonitor/src/rbcapp1_services_monitor.py
@@ -4,13 +4,26 @@ from rabbitmq_service import RabbitMQService
 from json_file_handler import JsonFileHandler
 
 def run():
-    serviceLog = JsonFileHandler.createFile(
-        httpd=HttpdService.getStatus(), 
-        postgres=PostgreSqlService.getStatus(), 
-        rabbitmq=RabbitMQService.getStatus()
-    )
+    try:
+        while True:
+            httpdServiceStatus = HttpdService.getStatus()
+            postgreSqlServiceStatus = PostgreSqlService.getStatus()
+            rabbitMQServiceStatus = RabbitMQService.getStatus()
 
-    return serviceLog
+            if (httpdServiceStatus and postgreSqlService and rabbitMQService):
+                applicationStatus = "UP"
+            else:
+                applicationStatus = "DOWN"
+
+            httpdServiceLog = JsonFileHandler.createLog("httpd", httpdServiceStatus)
+            postgreSqlServiceLog = JsonFileHandler.createLog("postgres", postgreSqlServiceStatus)
+            rabbitMQServiceLog = JsonFileHandler.createLog("rabbitmq", rabbitMQServiceStatus)
+
+            JsonFileHandler.createFile("httpd", httpdServiceLog)
+            JsonFileHandler.createFile("postgres", postgreSqlServiceLog)
+            JsonFileHandler.createFile("rabbitmq", rabbitMQServiceLog)
+    except Exception as e:
+        print(e)
 
 if __name__=="__main__":
     print(run())
