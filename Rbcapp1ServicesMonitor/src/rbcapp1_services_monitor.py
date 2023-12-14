2dfffc9ebea93295da49833c4ffa7fdcdd5e86c7
Santhosh <santhoshsunthar@gmail.com>
2023-12-14 08:45:48 +0530

    Added PostgreSqlService

--- Rbcapp1ServicesMonitor/src/rbcapp1_services_monitor.py
+++ Rbcapp1ServicesMonitor/src/rbcapp1_services_monitor.py
@@ -1,4 +1,6 @@
 from httpd_service import HttpdService
+from postqresql_service import PostgreSqlService
 
 if __name__=="__main__":
     print(HttpdService.getStatus())
+    print(PostgreSqlService.getStatus())
