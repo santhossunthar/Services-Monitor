631fcec36b9ea213134d61e381c24fc4999f8bea
Santhosh <santhoshsunthar@gmail.com>
2023-12-14 14:12:07 +0530

    Initialized RestApp

--- RestWebService/src/WebService/WebService/settings.py
+++ RestWebService/src/WebService/WebService/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2q*^igy%!y*^6x6ttib)w4@5e4%nd7+wt+d*ca0q7*z8qducm8'
+SECRET_KEY = 'django-insecure-(qr1q+9!ke5viw_q(k+l)7+gbc&ejmc6=ql-bzb*y&^-7exocx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'RestApp',
+    'django_restframework',
 ]
 
 MIDDLEWARE = [
--- RestWebService/src/WebService/WebService/urls.py
+++ RestWebService/src/WebService/WebService/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from RestApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/', views.getServiceLogData),
+    path('healthcheck/', views.checkHealth),
+    path('healthcheck/status/', checkHealthStatus),
 ]
