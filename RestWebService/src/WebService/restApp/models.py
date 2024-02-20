2ad2e043abd943708128154bfd3e20fbf6004a7f
santhossunthar <santhoshsunthar@gmail.com>
2024-02-21 02:10:47 +0530

    Fixed accepting and sending log data

--- RestWebService/src/WebService/restApp/models.py
+++ RestWebService/src/WebService/restApp/models.py
@@ -1,8 +1,1 @@
 from django.db import models
-from elasticsearch_dsl import Document, Text, Keyword
-
-# Create your models here.
-
-class LogEntry(models.Model):
-    timestamp = models.DateTimeField(auto_now_add=True)
-    message = models.TextField()
--- RestWebService/src/WebService/restApp/views.py
+++ RestWebService/src/WebService/restApp/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
-from rest_framework import views, serializers, response
-from .models import LogEntry
+from rest_framework import response
 from rest_framework.decorators import api_view
 import requests
 
 @api_view(['POST'])
 def addServiceLogData(request):
-    data = request.data
-    response = requests.post('http://localhost:5601', json=logs)
+    log_data = request.data
+    response = requests.post('http://localhost:5601', json=log_data)
+    return response
